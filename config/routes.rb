89a6200dbfd50cf3c50fb5860b914245bc3c968f
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-03-01 18:29:31 -0700

    Completed run in 40 minutes with over 50% notes

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+  get 'sign-out', to: 'authentication#destroy'
+  get 'sign-in', to: 'authentication#new'
+  post 'sign-in', to: 'authentication#create'
+
+end
